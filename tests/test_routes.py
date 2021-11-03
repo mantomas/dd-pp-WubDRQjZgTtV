7e8bec77a0141ea06e409d67b67beaee4ec972e0
Tom M <tomasman1979@gmail.com>
2021-11-03 18:29:15 +0100

    new tast cases for routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,11 +2,36 @@ from app.models import User
 
 
 def test_index(client):
-    """not logged in user
-    / or /index should redirect (302)
     """
-    response = client.post('/')
+    GIVEN non loged in user/client
+    WHEN route is called
+    THEN appropriate response
+    """
+    response = client.get('/')
+    # homepage - redirect
     assert response.status_code == 302
   
-    response = client.post('/index')
+    response = client.get('/index')
+    # alternate homepage - redirect
+
+    assert response.status_code == 302
+
+    response = client.get('/auth/login')
+    # login - OK
+    assert response.status_code == 200
+
+    response = client.get('/auth/register')
+    # registration - OK
+    assert response.status_code == 200
+
+    response = client.get('/auth/logout')
+    # logout - redirect
+    assert response.status_code == 302
+
+    response = client.get('/addtask')
+    # add new task - redirect
+    assert response.status_code == 302
+
+    response = client.get('/finished')
+    # finished tasks page - redirect
     assert response.status_code == 302
